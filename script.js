205368807f30a53eda6d758206860b8a96a9a6d4
scarvey01 <110380822+scarvey01@users.noreply.github.com>
2025-09-14 18:31:51 -0400

    image flip (halfway to spinning animation)
    images flip right now i need to get them to actually spin tho

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 const slotsDiv = document.querySelector("#slots");
 const buttonContainer = document.querySelector("#button_container");
 
-var buttons = [];
-
 // slot variables
 var slots = [];
+var slotCount = 0;
 var lastSpinTime = 0;
 const slotSymbols = ["cherry", "lemon", "grapes", "bar", "seven"];
-const slotSymbolPaths = {
-	"none": "",
-	"cherry": "/assets/cherry.png",
-	"lemon": "/assets/lemon.png",
-	"grapes": "/assets/grapes.png",
-	"bar": "/assets/bar.png",
-	"seven": "/assets/seven.png",
-}
+const slotTime = 3000 // 3 seconds in milliseconds
 
 // money variables
 var money = 0;
@@ -37,11 +29,10 @@ class Slot {
 	/** Creates a new slot and appends it to the slots array. */
 	constructor() {
 		this.id = slots.length;
-		this.result = "none";
+		this.result;
 
 		this.element = document.createElement("img");
 		this.element.className = "slot";
-		this.element.src = slotSymbolPaths[this.result];
 		slotsDiv.appendChild(this.element);
 
 		slots.push(this);
@@ -49,16 +40,19 @@ class Slot {
 
 	spin() {
 		this.result = slotSymbols[getRandomInt(0, slotSymbols.length)];
-		this.element.src = slotSymbolPaths[this.result];
+		//this.element.innerHTML = "<p>" + this.result + "</p>";
+		this.element.src = "./assets/" + this.result + ".png";
 		console.log("slot: " + this.id + "\n" + "result: " + this.result);
 	}
 }
 
-function spinSlots() {
+/*function spinSlots() {
 	for (let i = 0; i < slots.length; i++) {
 		slots[i].spin();
 	}
-}
+}*/
+
+var buttons = [];
 
 function createButton(_text, _id, _class, _function) {
 	let _button = document.createElement("button");
@@ -67,12 +61,48 @@ function createButton(_text, _id, _class, _function) {
 	_button.class = _class;
 	_button.onclick = _function;
 	buttons.push(_button);
-	buttonContainer.appendChild(_button);
 	return _button;
 }
 
-var spinButton = createButton("SPIN", "spin-button", "", () => { spinSlots() });
+// spinny animation (just swaps images for rn)
+function spinSlotsAnimated(){
+	const spinsPerSlot = 10;
+	const spinSpeed = 100; // time between changes (possible upgrade?)
+
+	slots.forEach(slot => 
+		{
+		let spinCount = 0;
+		function spinOnce(){
+			const tempResult = slotSymbols[getRandomInt(0, slotSymbols.length)];
+			slot.element.src = "./assets/" + tempResult + ".png"
+			spinCount++;
+
+			if (spinCount < spinsPerSlot) {
+				setTimeout(spinOnce, spinSpeed);
+			}
+			else {
+				//final result
+				slot.result = slotSymbols[getRandomInt(0, slotSymbols.length)];
+				slot.element.src = "./assets/" + slot.result + ".png";
+			}
+		}
+		spinOnce();
+	});
+}
+
+var spinButton = createButton("SPIN", "spin_button", "", () => { spinSlotsAnimated() });
 var newSlotButton = createButton("NEW SLOT", "new-slot-button", "", () => { new Slot() });
 
+function populateUI() {
+	for (let i = 0; i < buttons.length; i++) {
+		buttonContainer.appendChild(buttons[i]);
+	}
+}
+
+populateUI();
+
+//new Slot(slotCount);
 new Slot();
 spinSlots();
+
+setInterval(spinSlots, slotTime);
